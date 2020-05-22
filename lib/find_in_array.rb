3cf180fdf1604a7174cd1ed6aefca93c393da39f
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 15:51:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -10,6 +10,9 @@ def find_element_index(array, value_to_find)
   return nil
 end
 
+#While the below is cleaner, I think the above might be a more efficient method.
+#It won't do the extra work unless the array truly contains the value to find. 
+
 #for i in 0...(array.length)
 #  if array[i] == value_to_find
 #    return i
